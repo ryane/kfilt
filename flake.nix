85ff6c7b5ce87944fe54bd937e464820a5fb7fb2
ryane <ryanesc@gmail.com>
2025-11-30 16:22:33 -0500

    fixup flake

--- flake.nix
+++ flake.nix
@@ -6,32 +6,48 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     let
-      kfilt = pkgs:
+      kfilt =
+        pkgs:
         pkgs.buildGoModule rec {
           name = "kfilt";
           version = self.shortRev or "dirty";
           src = ./.;
           # this needs to be changed any time there is a change in go.mod
           # dependencies
-          vendorHash = null;
+          vendorHash = "sha256-TR6DZ8jV2InNT0IkFurESWT+2F4NDy2lRYhAMy0/h5c=";
           nativeBuildInputs = [ ];
-          CGO_ENABLED = 0;
+          env = {
+            CGO_ENABLED = 0;
+          };
           doCheck = false;
           ldflags = [
-            "-s -w -X github.com/ryane/kfilt/cmd.Version=${version} -X github.com/ryane/kfilt/cmd.GitCommit=${version}"
+            "-s"
+            "-w"
+            "-X github.com/ryane/kfilt/cmd.Version=${version}"
+            "-X github.com/ryane/kfilt/cmd.GitCommit=${version}"
           ];
           excludedPackages = [ "plugin/kustomize" ];
         };
-      flakeForSystem = nixpkgs: system:
+      flakeForSystem =
+        nixpkgs: system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           kf = kfilt pkgs;
-        in {
-          packages = { kfilt = kf; };
+        in
+        {
+          packages = {
+            kfilt = kf;
+          };
           devShell = pkgs.mkShell { packages = with pkgs; [ curl ]; };
         };
-    in flake-utils.lib.eachDefaultSystem
-    (system: flakeForSystem nixpkgs system);
+    in
+    flake-utils.lib.eachDefaultSystem (system: flakeForSystem nixpkgs system);
 }
